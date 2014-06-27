be540121dbd70ac84184508b85ecdf019db706c6
skearnes <kearnes@stanford.edu>
2014-06-26 17:54:04 -0700

    Update test docstrings

--- pubchem/tests/test_pug.py
+++ pubchem/tests/test_pug.py
@@ -5,18 +5,18 @@ from pubchem import pug
 
 
 def test_cid():
-    """Test CID download."""
+    """Test CID request."""
     p = pug.PUG(delay=3)
     p.get_ids([2244])
 
 
 def test_sid():
-    """Test SID download."""
+    """Test SID request."""
     p = pug.PUG(delay=3)
     p.get_ids([179038559], sids=True)
 
 
 def test_3d():
-    """Test 3D structure download."""
+    """Test 3D structure request."""
     p = pug.PUG(delay=3)
     p.get_ids([2244], use_3d=True, n_conformers=10)
