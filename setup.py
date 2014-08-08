2f7e2cf3ba25ebfbeb550119befd29bd408dac25
Steven Kearnes <skearnes@gmail.com>
2014-08-08 12:23:19 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup, find_packages
 
 def main():
     setup(
-        name='pubchem',
+        name='pubchem_utils',
         version='0.1',
         license='3-clause BSD',
-        url='https://github.com/skearnes/pubchem',
+        url='https://github.com/skearnes/pubchem-utils',
         description='Utilities for interacting with PubChem',
         packages=find_packages(),
     )
